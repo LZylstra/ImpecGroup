2d37bcd8083172226a07667f5fb79f6801b4b861
LZylstra <lindseyzylstra@gmail.com>
2020-06-02 16:35:39 -0700

    updates to top bar

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -5,13 +5,16 @@ function LandingPage() {
   return (
     <div className="LandingPage">
       <div className="LP-covid">
-        <p>COVID-19 Return to Work Solutions</p>
-        <p>
+        <p id="LP-covid-header">COVID-19 Return to Work Solutions</p>
+        <p id="LP-covid-body">
           Impec Group offers full cycle end-to-end services to help you
           implement your return to work and return to office strategy. This
           includes creating flexible work policies, designing offices for social
           distancing, planning rotation schedules, and many more to plan for our
           new normal.
+        </p>
+        <p id="LP-covid-footer">
+          For any and all facility management services, we're here to help.
         </p>
       </div>
     </div>
